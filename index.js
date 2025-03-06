7ecba882b3783636580698a61e94d2cd826b2dca
forannn <forann133@gmail.com>
2025-03-06 11:52:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const incrementBtn = document.getElementById("increment-btn")
-let countEl = document.getElementById("count-el")
-const saveBtn = document.getElementById("save-btn")
-let saveEl = document.getElementById("save-el")
+const incrementBtn = document.getElementById("increment-btn");
+let countEl = document.getElementById("count-el");
+const saveBtn = document.getElementById("save-btn");
+let saveEl = document.getElementById("save-el");
 
-let count = 0
-let saveElArray = []
-incrementBtn.addEventListener("click", function() {
-    count += 1
-    countEl.textContent = count
-})
+let count = 0;
+let saveElArray = [];
+incrementBtn.addEventListener("click", function () {
+  count += 1;
+  countEl.textContent = count;
+});
 
-
-saveBtn.addEventListener("click", function() {
-    if ( count !== 0) {
-        saveElArray.push(count)
-        let save = "Previous entries: " + saveElArray.join(" - ")
-        saveEl.textContent = save
-        countEl.textContent = 0
-        count = 0
+saveBtn.addEventListener("click", function () {
+  if (count !== 0) {
+    saveElArray.push(count);
+    if (saveElArray.length - 1 === 0) {
+      saveEl.textContent += count;
+    } else {
+      saveEl.textContent += " - " + count;
     }
-})
+  }
+  countEl.textContent = 0;
+  count = 0;
+});
